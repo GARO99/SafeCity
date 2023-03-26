7809278e08990c77bb294abeae702e71c0c12e1d
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2023-03-26 17:53:07 -0500

    architecture app

---
+++ src/app/UI/modules/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from '@UIModules/auth/auth-routing.module';
+import { SignInComponent } from '@UIModules/auth/pages/sign-in/sign-in.component';
+import { SignUpComponent } from '@UIModules/auth/pages/sign-up/sign-up.component';
+import { ShareModule } from '@UIShare/share.module';
+import { IonicModule } from '@ionic/angular';
+
+
+@NgModule({
+  declarations: [
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,    
+    IonicModule.forRoot(),
+    ShareModule
+  ]
+})
+export class AuthModule { }
