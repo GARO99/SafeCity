163c8902e377156f4092e44ef961fe1b5eecf10f
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2023-03-26 17:53:07 -0500

    architecture app

---
+++ src/app/UI/share/share.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  exports:[],
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShareModule { }
