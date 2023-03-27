ef3704d9d8d520a79f58bf061dd61689c7271ae1
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2023-03-27 00:13:57 -0500

    Login page and validations has been developed

--- src/app/UI/modules/auth/pages/sign-in/sign-in.component.ts
+++ src/app/UI/modules/auth/pages/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  signInForm!: FormGroup;
+  validations: any = {
+    'email': [
+      { type: 'required', message: 'El email es requerido.' },
+      { type: 'pattern', message: 'El email debe ser válido' },
+    ],
+    'password': [
+      { type:'required', message: 'La contraseña es requerida.' },
+      { type: 'pattern', message: 'La contraseña debe tener lo menos 6 caracteres, un numero y un carácter especial' },
+    ]
+  }
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.FormBuild();
+  }
+
+  private FormBuild(): void {
+    this.signInForm = this.fb.group({
+      email: [null, [Validators.required, Validators.pattern(/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/)]],
+      password: [null, [
+        Validators.required,
+        Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{6,}$/)
+      ]]
+    });
+  } 
+
+  onSubmit(e: Event): void {
+    e.preventDefault();
+    if (this.signInForm.valid) {
+      console.log('Works');
+    }
+  }
 
 }
