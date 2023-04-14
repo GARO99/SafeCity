20e2dab664de8d07610358578de410b336b1a892
Kevin Sierra <niichols1993@gmail.com>
2023-04-14 09:40:41 -0500

    Kevin2

--- src/app/UI/modules/auth/auth.module.ts
+++ src/app/UI/modules/auth/auth.module.ts
@@ -7,17 +7,13 @@ import { SignUpComponent } from '@UIModules/auth/pages/sign-up/sign-up.component
 import { ShareModule } from '@UIShare/share.module';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HistorialComponente } from './pages/historial/historial.page';
-import { Tab1Page } from './pages/tab1/tab1.page';
-import { Tab2Page } from './pages/tab2/tab2.page';
-import { Tab3Page } from './pages/tab3/tab3.page';
-import { TabsPage } from './pages/tabs/tabs.page';
+
 
 @NgModule({
   declarations: [
     SignInComponent,
     SignUpComponent,
-    HistorialComponente,
+    //HistorialComponente,
     //Tab1Page,
     //Tab2Page,  
     //Tab3Page,
--- src/app/UI/modules/auth/pages/historial/historial.page.ts
+++ src/app/UI/modules/auth/pages/historial/historial.page.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { CustomValidators } from '@UIUtils/custom-validators';
 
 @Component({
   selector: 'app-historial',
   templateUrl: './historial.page.html',
   styleUrls: ['./historial.page.scss'],
-  //standalone: true,
-  // imports: [IonicModule, ExploreContainerComponent],
+  standalone: true,
+  imports: [IonicModule, ExploreContainerComponent],
 })
-export class HistorialComponente {
+export class HistorialPage {
   constructor() {}
 }
 
